20771cc8189c8a877f8cf72f6d2b0cbb97cedef1
Hugh Scott <hughscott2002@yahoo.com>
2025-10-19 14:41:47 -0500

    Fraud Detection Service

--- 5-fraud-detection/src/server/handlers/risk_assessment.go
+++ 5-fraud-detection/src/server/handlers/risk_assessment.go
@@ -51,7 +51,7 @@ func HandlerHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
-		"status": "healthy",
+		"status":  "healthy",
 		"service": "fraud-detection",
 	})
 }
